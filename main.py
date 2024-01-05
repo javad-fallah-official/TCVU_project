f5ae949e4f04f6985c04adc58b97a21b4d31cde6
javad_fallah_official <javad.fallah.official@gmail.com>
2024-01-05 08:57:36 -0500

    all class List Boxes added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import StringVar, Listbox
 from tkinter import filedialog, ttk, Button, Label, LabelFrame, Entry
 import tkinter as tk
-import owlready2
+from owlready2 import *
 
 
 class NetworkSecurityOntologyApp:
@@ -11,7 +11,11 @@ class NetworkSecurityOntologyApp:
         self.master.geometry("1920x1080")
 
         # Variables
-        self.initialize_variables()
+        self.prefix = StringVar()
+        self.onto_path = StringVar()
+        self.myOntoPath = list()
+        self.onto = StringVar()
+        self.Listbox = []
 
         # Create tabs
         self.create_tabs()
@@ -33,79 +37,89 @@ class NetworkSecurityOntologyApp:
     def create_view_tab(self, tab_control):
         view_tab = ttk.Frame(tab_control)
         tab_control.add(view_tab, text="Standards")
+
         # Load File UI
-        # load file GroupBox
         loadfileGroupBox = LabelFrame(view_tab, text="Load owl file")
         loadfileGroupBox.place(x=6, y=6, width=650, height=100)
+
         # open file button
-        lblBrowse = Label(view_tab, text="select file : ", anchor="e")
+        lblBrowse = Label(view_tab, text="Select file : ", anchor="e")
         lblBrowse.place(x=12, y=60)
-        btnBrowse = Button(view_tab, text="file", command=self.load_file)
+
+        btnBrowse = Button(view_tab, text="File", command=self.load_file)
         btnBrowse.place(x=90, y=60, width=90)
+
         tiplbl = Label(
             view_tab, text="Please make sure to enter Prefix correctly!!!")
         tiplbl.place(x=210, y=60)
+
         # Prefix input
         lblBrowse2 = Label(view_tab, text="Prefix : ", anchor="e")
         lblBrowse2.place(x=12, y=27)
         self.prefixBox = Entry(view_tab, textvariable=self.prefix, width=90)
-        # default value
         self.prefixBox.insert(0, "http://www.semantic.org/hamidzadeh/SOSM")
         self.prefixBox.place(x=72, y=27)
 
         # ListBox and view UI
-        # View GroupBox
         viewGroupBox = LabelFrame(view_tab, text="View")
         viewGroupBox.place(x=6, y=120, width=1890, height=810)
 
-        label_list = []
-        label_list.insert(0, Label(view_tab, text="Classes:"))
-        label_list.insert(1, Label(view_tab, text="Subclass:"))
-        label_list.insert(2, Label(view_tab, text="Controllers"))
-        label_list.insert(3, Label(view_tab, text="Sub Controls"))
-
+        label1 = Label(view_tab, text="Classes:")
+        label1.place(x=54, y=180)
         start_x = 54
         start_y = 210
         list_width = 54
+
         for listnum in range(0, 5):
-            self.Listbox.insert(listnum, Listbox(
-                view_tab, height=30, width=list_width))
+            self.Listbox.append(Listbox(view_tab, height=30, width=list_width))
+            self.Listbox[listnum].bind(
+                '<<ListboxSelect>>', self.show_subclasses)
             start_x += list_width + 300 if listnum != 0 else 0
-            self.Listbox[listnum].place(
-                x=start_x, y=start_y)
+            self.Listbox[listnum].place(x=start_x, y=start_y)
 
     # loading file and datas
     def load_file(self):
         # choosing file
-        self.onto_path = self.file_open_box()
+        self.onto_path.set(self.file_open_box())
         # loading owl
-        self.onto = owlready2.get_ontology(self.onto_path).load()
+        self.onto = owlready2.get_ontology(self.onto_path.get()).load()
         # set prefix
-        self.prefix = self.prefixBox.get()
+        self.prefix.set(self.prefixBox.get())
         self.extract_classes()
 
     # file select window
     def file_open_box(self):
-        root = tk.Tk()
-        root.withdraw()  # Hide the main window
-
         file_path = filedialog.askopenfilename(
-            filetypes=[("OWL Files", "*.owl")], title="Select an OWL File"
-        )
+            filetypes=[("OWL Files", "*.owl")], title="Select an OWL File")
         return file_path
 
     def extract_classes(self):
-        for subclass in self.extract_Subclasses('Thing'):
-            self.Listbox[0].insert("end", subclass.name)
+        namespace = self.onto.get_namespace("http://www.w3.org/2002/07/owl")
+        class_name = getattr(namespace, 'Thing', None)
+        subclasses = list(class_name.subclasses())
+        for subclass in subclasses:
+            self.Listbox[0].insert("end", str(subclass.name).replace('_', ' '))
 
     def extract_Subclasses(self, parent):
-       # Get the namespace from the base IRI
-        namespace = self.onto.get_namespace(
-            self.prefix)
-        # Get the class from the namespace
-        class_name = getattr(namespace,  parent, None)
+        namespace = self.onto.get_namespace(self.prefix.get())
+        class_name = getattr(namespace, parent, None)
         subclasses = list(class_name.subclasses())
         return subclasses
+
+    def show_subclasses(self, event):
+        for i, list in enumerate(self.Listbox):
+            if list.curselection() != ():
+                num = i
+        if num != 4:
+            self.Listbox[num + 1].delete(0, tk.END)
+            selected_index = self.Listbox[num].curselection()
+            if selected_index:
+                selected_item = self.Listbox[num].get(selected_index[0])
+                selected_item = str(selected_item).replace(' ', '_')
+                subclasses = self.extract_Subclasses(selected_item)
+                for subclass in subclasses:
+                    self.Listbox[num + 1].insert("end",
+                                                 str(subclass.name).replace('_', ' '))
 
 
 def main():
